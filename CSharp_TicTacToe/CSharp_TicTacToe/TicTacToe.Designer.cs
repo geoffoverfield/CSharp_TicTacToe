d42d2d7c8f29fb72ccd314856fdb6e3a720d7f5b
Geoff Overfield <goverfield@fabpro1.com>
2019-03-11 01:06:57 -0600

    Initial Build.
    Needs further testing with logic - sending or reading wrong values from row/column grids & sending false negatives.

---
+++ CSharp_TicTacToe/CSharp_TicTacToe/TicTacToe.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace CSharp_TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TicTacToe));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.playToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.creditToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lbl9 = new System.Windows.Forms.Label();
+            this.lblPlayerTurn = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playToolStripMenuItem,
+            this.creditToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // playToolStripMenuItem
+            // 
+            this.playToolStripMenuItem.Name = "playToolStripMenuItem";
+            this.playToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.playToolStripMenuItem.Text = "Play";
+            this.playToolStripMenuItem.Click += new System.EventHandler(this.playToolStripMenuItem_Click);
+            // 
+            // creditToolStripMenuItem
+            // 
+            this.creditToolStripMenuItem.Name = "creditToolStripMenuItem";
+            this.creditToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.creditToolStripMenuItem.Text = "Credits";
+            this.creditToolStripMenuItem.Click += new System.EventHandler(this.creditToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.BackColor = System.Drawing.Color.LightGray;
+            this.lbl1.Location = new System.Drawing.Point(28, 42);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(150, 150);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.BackColor = System.Drawing.Color.LightGray;
+            this.lbl2.Location = new System.Drawing.Point(208, 42);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(150, 150);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Click += new System.EventHandler(this.lbl2_Click);
+            // 
+            // lbl3
+            // 
+            this.lbl3.BackColor = System.Drawing.Color.LightGray;
+            this.lbl3.Location = new System.Drawing.Point(388, 42);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(150, 150);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Click += new System.EventHandler(this.lbl3_Click);
+            // 
+            // lbl4
+            // 
+            this.lbl4.BackColor = System.Drawing.Color.LightGray;
+            this.lbl4.Location = new System.Drawing.Point(28, 223);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(150, 150);
+            this.lbl4.TabIndex = 5;
+            this.lbl4.Click += new System.EventHandler(this.lbl4_Click);
+            // 
+            // lbl5
+            // 
+            this.lbl5.BackColor = System.Drawing.Color.LightGray;
+            this.lbl5.Location = new System.Drawing.Point(208, 223);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(150, 150);
+            this.lbl5.TabIndex = 6;
+            this.lbl5.Click += new System.EventHandler(this.lbl5_Click);
+            // 
+            // lbl6
+            // 
+            this.lbl6.BackColor = System.Drawing.Color.LightGray;
+            this.lbl6.Location = new System.Drawing.Point(388, 223);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(150, 150);
+            this.lbl6.TabIndex = 7;
+            this.lbl6.Click += new System.EventHandler(this.lbl6_Click);
+            // 
+            // lbl7
+            // 
+            this.lbl7.BackColor = System.Drawing.Color.LightGray;
+            this.lbl7.Location = new System.Drawing.Point(28, 404);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(150, 150);
+            this.lbl7.TabIndex = 8;
+            this.lbl7.Click += new System.EventHandler(this.lbl7_Click);
+            // 
+            // lbl8
+            // 
+            this.lbl8.BackColor = System.Drawing.Color.LightGray;
+            this.lbl8.Location = new System.Drawing.Point(208, 404);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(150, 150);
+            this.lbl8.TabIndex = 9;
+            this.lbl8.Click += new System.EventHandler(this.lbl8_Click);
+            // 
+            // lbl9
+            // 
+            this.lbl9.BackColor = System.Drawing.Color.LightGray;
+            this.lbl9.Location = new System.Drawing.Point(388, 404);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(150, 150);
+            this.lbl9.TabIndex = 10;
+            this.lbl9.Click += new System.EventHandler(this.lbl9_Click);
+            // 
+            // lblPlayerTurn
+            // 
+            this.lblPlayerTurn.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayerTurn.Font = new System.Drawing.Font("Kwajong", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerTurn.Location = new System.Drawing.Point(31, 566);
+            this.lblPlayerTurn.Name = "lblPlayerTurn";
+            this.lblPlayerTurn.Size = new System.Drawing.Size(507, 56);
+            this.lblPlayerTurn.TabIndex = 11;
+            this.lblPlayerTurn.Text = "READY!";
+            this.lblPlayerTurn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(185, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 547);
+            this.label1.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(365, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 547);
+            this.label2.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(12, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(541, 15);
+            this.label3.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Location = new System.Drawing.Point(12, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(541, 15);
+            this.label4.TabIndex = 15;
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(584, 631);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPlayerTurn);
+            this.Controls.Add(this.lbl9);
+            this.Controls.Add(this.lbl8);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TicTacToe";
+            this.Text = "Geoff\'s Tic Tac Toe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem creditToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl9;
+        private System.Windows.Forms.Label lblPlayerTurn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CSharp_TicTacToe/CSharp_TicTacToe/TicTacToe.cs
@@ -0,0 +1,416 @@
+﻿// Geoff Overfield
+// Basic 2 Player Tic Tac Toe game
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CSharp_TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        enum Winner { Player1, Player2};
+        enum PlayerTurn { Player1, Player2 };
+        Bitmap[] player1Imgs, player2Imgs;
+        Random m_pRandom;
+        Dictionary<string, string[]> dRowsAndColumns;
+
+        Winner eWinner;
+        PlayerTurn eTurn;
+        bool m_bGameStarted, m_bGameWon;
+        
+        public TicTacToe()
+        {
+            InitializeComponent();
+            loadImages();
+            createDictionary();
+
+            m_pRandom = new Random();
+            m_bGameStarted = m_bGameWon = false;
+        }
+
+        private bool checkDiagonals()
+        {
+            string[] arLeftToRight = new string[3];
+            string[] arRightToLeft = new string[3];
+
+            arLeftToRight[0] = dRowsAndColumns["row1"][0];
+            arLeftToRight[1] = dRowsAndColumns["row2"][1];
+            arLeftToRight[2] = dRowsAndColumns["row3"][2];
+            arRightToLeft[0] = dRowsAndColumns["row1"][2];
+            arRightToLeft[1] = dRowsAndColumns["row2"][1];
+            arRightToLeft[2] = dRowsAndColumns["row3"][0];
+
+            foreach (var sVal in arLeftToRight)
+                if (string.IsNullOrEmpty(sVal)) return false;
+            foreach (var sVal in arRightToLeft)
+                if (string.IsNullOrEmpty(sVal)) return false;
+
+            if (arLeftToRight[0] == arLeftToRight[1] &&
+                arLeftToRight[1] == arLeftToRight[2]) return true;
+            else if (arRightToLeft[0] == arRightToLeft[1] &&
+                arRightToLeft[1] == arRightToLeft[2]) return true;
+
+            return false;
+        }
+
+        private void createDictionary()
+        {
+            dRowsAndColumns = new Dictionary<string, string[]>();
+            List<string> lRowsAndColumns = new List<string>()
+            {
+                "row1",
+                "row2",
+                "row3",
+                "column1",
+                "column2",
+                "column3"
+            };
+            foreach (var sValue in lRowsAndColumns)
+                dRowsAndColumns.Add(sValue, new string[3]);
+        }
+
+        private void loadImages()
+        {
+            player1Imgs = new Bitmap[6];
+            player2Imgs = new Bitmap[6];
+
+            int i = 0;
+            player1Imgs[i++] = Properties.Resources.O1;
+            player1Imgs[i++] = Properties.Resources.O2;
+            player1Imgs[i++] = Properties.Resources.O3;
+            player1Imgs[i++] = Properties.Resources.O4;
+            player1Imgs[i++] = Properties.Resources.O5;
+            player1Imgs[i++] = Properties.Resources.O6;
+            i = 0;
+            player2Imgs[i++] = Properties.Resources.X1;
+            player2Imgs[i++] = Properties.Resources.X2;
+            player2Imgs[i++] = Properties.Resources.X3;
+            player2Imgs[i++] = Properties.Resources.X4;
+            player2Imgs[i++] = Properties.Resources.X5;
+            player2Imgs[i++] = Properties.Resources.X6;
+        }
+
+        private void nextPlayer()
+        {
+            if (eTurn == PlayerTurn.Player1)
+                eTurn = PlayerTurn.Player2;
+            else eTurn = PlayerTurn.Player1;
+        }
+
+        private void updateTurnLabel()
+        {
+            if (!m_bGameWon)
+            {
+                switch (eTurn)
+                {
+                    case PlayerTurn.Player1:
+                        lblPlayerTurn.Text = "Player 1s Turn";
+                        break;
+                    case PlayerTurn.Player2:
+                        lblPlayerTurn.Text = "Player 2s Turn";
+                        break;
+                }
+            }
+            else
+            {
+                switch (eWinner)
+                {
+                    case Winner.Player1:
+                        lblPlayerTurn.Text = "Player 1 Wins!";
+                        break;
+                    case Winner.Player2:
+                        lblPlayerTurn.Text = "Player 2 Wins!";
+                        break;
+                }
+            }
+        }
+
+        private bool winnerExists()
+        {
+            foreach (var kvp in dRowsAndColumns)
+            {
+                bool bSkip = false;
+                foreach (var sVal in kvp.Value)
+                    if (string.IsNullOrEmpty(sVal))
+                    {
+                        bSkip = true;
+                        break;
+                    }
+                if (bSkip) continue;
+                if (kvp.Value[0] == kvp.Value[1] &&
+                    kvp.Value[1] == kvp.Value[2])
+                {
+                    m_bGameWon = true;
+                    break;
+                }
+            }
+            if (!m_bGameWon) m_bGameWon = checkDiagonals();
+
+            return m_bGameWon;
+        }
+        
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+            if (lbl1.Image != null || !m_bGameStarted || m_bGameWon) return;
+
+            switch (eTurn)
+            {
+                case PlayerTurn.Player1:
+                    lbl1.Image = player1Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row1"][0] = dRowsAndColumns["column1"][0] = "x";
+                    break;
+                case PlayerTurn.Player2:
+                    lbl1.Image = player2Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row1"][0] = dRowsAndColumns["column1"][0] = "o";
+                    break;
+            }
+            nextPlayer();
+            if (winnerExists())
+            {
+                m_bGameStarted = false;
+                m_bGameWon = true;
+                eWinner = (Winner)eTurn;
+            }
+            updateTurnLabel();
+        }
+
+        private void lbl2_Click(object sender, EventArgs e)
+        {
+            if (lbl2.Image != null || !m_bGameStarted || m_bGameWon) return;
+
+            switch (eTurn)
+            {
+                case PlayerTurn.Player1:
+                    lbl2.Image = player1Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row1"][1] = dRowsAndColumns["column2"][0] = "x";
+                    break;
+                case PlayerTurn.Player2:
+                    lbl2.Image = player2Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row1"][1] = dRowsAndColumns["column2"][0] = "o";
+                    break;
+            }
+            if (winnerExists())
+            {
+                m_bGameStarted = false;
+                m_bGameWon = true;
+                eWinner = (Winner)eTurn;
+            }
+            nextPlayer();
+            updateTurnLabel();
+        }
+
+        private void lbl3_Click(object sender, EventArgs e)
+        {
+            if (lbl3.Image != null || !m_bGameStarted || m_bGameWon) return;
+
+            switch (eTurn)
+            {
+                case PlayerTurn.Player1:
+                    lbl3.Image = player1Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row1"][2] = dRowsAndColumns["column3"][0] = "x";
+                    break;
+                case PlayerTurn.Player2:
+                    lbl3.Image = player2Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row1"][2] = dRowsAndColumns["column3"][0] = "o";
+                    break;
+            }
+            nextPlayer();
+            if (winnerExists())
+            {
+                m_bGameStarted = false;
+                m_bGameWon = true;
+                eWinner = (Winner)eTurn;
+            }
+            updateTurnLabel();
+        }
+
+        private void lbl4_Click(object sender, EventArgs e)
+        {
+            if (lbl4.Image != null || !m_bGameStarted || m_bGameWon) return;
+
+            switch (eTurn)
+            {
+                case PlayerTurn.Player1:
+                    lbl4.Image = player1Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row2"][0] = dRowsAndColumns["column1"][1] = "x";
+                    break;
+                case PlayerTurn.Player2:
+                    lbl4.Image = player2Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row2"][0] = dRowsAndColumns["column1"][1] = "o";
+                    break;
+            }
+            nextPlayer();
+            if (winnerExists())
+            {
+                m_bGameStarted = false;
+                m_bGameWon = true;
+                eWinner = (Winner)eTurn;
+            }
+            updateTurnLabel();
+        }
+
+        private void lbl5_Click(object sender, EventArgs e)
+        {
+            if (lbl5.Image != null || !m_bGameStarted || m_bGameWon) return;
+
+            switch (eTurn)
+            {
+                case PlayerTurn.Player1:
+                    lbl5.Image = player1Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row2"][1] = dRowsAndColumns["column2"][1] = "x";
+                    break;
+                case PlayerTurn.Player2:
+                    lbl5.Image = player2Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row2"][1] = dRowsAndColumns["column2"][1] = "o";
+                    break;
+            }
+            nextPlayer();
+            if (winnerExists())
+            {
+                m_bGameStarted = false;
+                m_bGameWon = true;
+                eWinner = (Winner)eTurn;
+            }
+            updateTurnLabel();
+        }
+
+        private void lbl6_Click(object sender, EventArgs e)
+        {
+            if (lbl6.Image != null || !m_bGameStarted || m_bGameWon) return;
+
+            switch (eTurn)
+            {
+                case PlayerTurn.Player1:
+                    lbl6.Image = player1Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row2"][2] = dRowsAndColumns["column3"][1] = "x";
+                    break;
+                case PlayerTurn.Player2:
+                    lbl6.Image = player2Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row2"][2] = dRowsAndColumns["column3"][1] = "o";
+                    break;
+            }
+            nextPlayer();
+            if (winnerExists())
+            {
+                m_bGameStarted = false;
+                m_bGameWon = true;
+                eWinner = (Winner)eTurn;
+            }
+            updateTurnLabel();
+        }
+
+        private void lbl7_Click(object sender, EventArgs e)
+        {
+            if (lbl7.Image != null || !m_bGameStarted || m_bGameWon) return;
+
+            switch (eTurn)
+            {
+                case PlayerTurn.Player1:
+                    lbl7.Image = player1Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row3"][0] = dRowsAndColumns["column1"][2] = "x";
+                    break;
+                case PlayerTurn.Player2:
+                    lbl7.Image = player2Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row3"][0] = dRowsAndColumns["column1"][2] = "o";
+                    break;
+            }
+            nextPlayer();
+            if (winnerExists())
+            {
+                m_bGameStarted = false;
+                m_bGameWon = true;
+                eWinner = (Winner)eTurn;
+            }
+            updateTurnLabel();
+        }
+
+        private void lbl8_Click(object sender, EventArgs e)
+        {
+            if (lbl8.Image != null || !m_bGameStarted || m_bGameWon) return;
+
+            switch (eTurn)
+            {
+                case PlayerTurn.Player1:
+                    lbl8.Image = player1Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row3"][1] = dRowsAndColumns["column2"][2] = "x";
+                    break;
+                case PlayerTurn.Player2:
+                    lbl8.Image = player2Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row3"][1] = dRowsAndColumns["column2"][2] = "o";
+                    break;
+            }
+            nextPlayer();
+            if (winnerExists())
+            {
+                m_bGameStarted = false;
+                m_bGameWon = true;
+                eWinner = (Winner)eTurn;
+            }
+            updateTurnLabel();
+        }
+
+        private void lbl9_Click(object sender, EventArgs e)
+        {
+            if (lbl9.Image != null || !m_bGameStarted || m_bGameWon) return;
+
+            switch (eTurn)
+            {
+                case PlayerTurn.Player1:
+                    lbl9.Image = player1Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row3"][2] = dRowsAndColumns["column3"][2] = "x";
+                    break;
+                case PlayerTurn.Player2:
+                    lbl9.Image = player2Imgs[m_pRandom.Next(0, 6)];
+                    dRowsAndColumns["row3"][2] = dRowsAndColumns["column3"][2] = "o";
+                    break;
+            }
+            nextPlayer();
+            if (winnerExists())
+            {
+                m_bGameStarted = false;
+                m_bGameWon = true;
+                eWinner = (Winner)eTurn;
+            }
+            updateTurnLabel();
+        }
+
+        private void playToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!m_bGameStarted)
+            {
+                m_bGameStarted = true;
+                eTurn = PlayerTurn.Player1;
+            }
+            else
+            {
+                if (!m_bGameWon)
+                {
+                    //Message to warn of game restart.
+                    if (MessageBox.Show("Game in Progress!", "You are about to restart your game.  Do you wish to continue?",
+                        MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation) == DialogResult.Cancel) return;
+                }
+                m_bGameWon = false;
+                m_bGameStarted = true;
+                eTurn = PlayerTurn.Player1;
+                createDictionary();
+                lbl1.Image = lbl2.Image = lbl3.Image = lbl4.Image = lbl5.Image =
+                    lbl6.Image = lbl7.Image = lbl8.Image = lbl9.Image = null;
+
+            }
+            updateTurnLabel();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void creditToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string sMsg = "Created, Designed and Programmed by Geoff Overfield.\n(c)[BOSS] Games 2015";
+            MessageBox.Show(sMsg);
+        }
+    }
+}
